920100c69b1c00afd729390f5b1349d92372e1dc
Vitória <vihlopes423@gmail.com>
2021-03-25 15:36:28 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require ("express")
+const app = express ()
+var bodyParser = require ('body-parser')
+var cookieParser = require ('cookie-parser')
+var path = require ('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set('view engine', "ejs")
+app.use(express.static(path.join(__dirname,"public")))
+app.get('/',function(req,res){
+    res.render('formulario.ejs')
+
+})
+app.post('/', function(req,res){
+    res.send("Olá mundo via Post!")
+
+})
+
+app.listen(3000 , function(){
+        console.log("Conexão Inicializada!")
+});
