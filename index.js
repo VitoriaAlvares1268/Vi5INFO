be29c534a1a48abdd0a3c3bfc6507022c6c55507
Vitória <vihlopes423@gmail.com>
2021-04-09 01:45:09 -0300

    Usando ejs nas páginas

--- index.js
+++ index.js
@@ -11,13 +11,49 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.set('view engine', "ejs")
 app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/i',function(req,res){
+    res.render('index.ejs', {})
+}) 
+app.get('/usuarios',function(req,res){
+        res.render('usuarios.ejs',{usuarios:[
+            {nome:'Vitória', email:'Vihlopes@gmail.com' },
+            {nome:'Mariana', email:'Marigomes@gmail.com' },
+            {nome:'Eduarda', email:'Eduardafarias@gmail.com' },
+            {nome:'Luciana', email:'Lucianaluiz@gmail.com' }
+
+        ]})
+}) 
+
+//Listar todos os dados
 app.get('/',function(req,res){
-    res.render('formulario.ejs')
+    res.render('list.ejs')
+}) 
 
+//listar os dados de acordo com filtro
+app.post('/',function(req,res){
+    res.render('list.ejs')
 })
-app.post('/', function(req,res){
-    res.send("Olá mundo via Post!")
 
+// Abrir tela add
+app.get('/add',function(req,res){
+    res.render('add.ejs')
+})
+// Salvvardados adicionados no banco  de dados
+app.post('/add',function(req,res){
+    res.render('add.ejs')
+})
+
+// Abrir tela add
+app.get('/edit',function(req,res){
+    res.render('edit.ejs')
+})
+// Salvar os dados editados no banco  de dados
+app.post('/edit',function(req,res){
+    res.render('edit.ejs')
+})
+app.get('/del]', function(req,res){
+    res.render('list.ejs')
 })
 
 app.listen(3000 , function(){
